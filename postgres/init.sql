8e7f9cb33aa3c797dcc6a1e4864f2d378b5a1eb3
Matheus Fideles <matheuss.fideles@hotmail.com>
2024-10-08 17:28:11 -0300

    update init

--- postgres/init.sql
+++ postgres/init.sql
@@ -12,30 +12,30 @@ CREATE TABLE invoice.person (
 
 CREATE TABLE invoice.invoicing (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    personId UUID NOT NULL,
+    taxId VARCHAR(11) NOT NULL UNIQUE,
     amount BIGINT NOT NULL
 );
 
-INSERT INTO invoice.person (taxId, name) VALUES ('12345678901', 'Ana Silva');
-INSERT INTO invoice.person (taxId, name) VALUES ('23456789012', 'Bruno Costa');
-INSERT INTO invoice.person (taxId, name) VALUES ('34567890123', 'Carla Pereira');
-INSERT INTO invoice.person (taxId, name) VALUES ('45678901234', 'Daniel Oliveira');
-INSERT INTO invoice.person (taxId, name) VALUES ('56789012345', 'Eduarda Santos');
-INSERT INTO invoice.person (taxId, name) VALUES ('67890123456', 'Felipe Almeida');
-INSERT INTO invoice.person (taxId, name) VALUES ('78901234567', 'Gabriela Souza');
-INSERT INTO invoice.person (taxId, name) VALUES ('89012345678', 'Henrique Lima');
-INSERT INTO invoice.person (taxId, name) VALUES ('90123456789', 'Isabella Rocha');
-INSERT INTO invoice.person (taxId, name) VALUES ('01234567890', 'João Mendes');
-INSERT INTO invoice.person (taxId, name) VALUES ('13579246810', 'Karina Nascimento');
-INSERT INTO invoice.person (taxId, name) VALUES ('24680135791', 'Lucas Martins');
-INSERT INTO invoice.person (taxId, name) VALUES ('35791357912', 'Mariana Torres');
-INSERT INTO invoice.person (taxId, name) VALUES ('46802468013', 'Nicolas Andrade');
-INSERT INTO invoice.person (taxId, name) VALUES ('57913579124', 'Olga Ferreira');
-INSERT INTO invoice.person (taxId, name) VALUES ('68024680235', 'Paulo Ribeiro');
-INSERT INTO invoice.person (taxId, name) VALUES ('79135791346', 'Quezia Almeida');
-INSERT INTO invoice.person (taxId, name) VALUES ('80246802457', 'Ricardo Teixeira');
-INSERT INTO invoice.person (taxId, name) VALUES ('91357913568', 'Sofia Pacheco');
-INSERT INTO invoice.person (taxId, name) VALUES ('02468024679', 'Tiago Campos');
+INSERT INTO invoice.person (taxId, name) VALUES ('85666113062', 'Ana Silva');
+INSERT INTO invoice.person (taxId, name) VALUES ('65447617022', 'Bruno Costa');
+INSERT INTO invoice.person (taxId, name) VALUES ('29031528021', 'Carla Pereira');
+INSERT INTO invoice.person (taxId, name) VALUES ('28703693015', 'Daniel Oliveira');
+INSERT INTO invoice.person (taxId, name) VALUES ('62181001030', 'Eduarda Santos');
+INSERT INTO invoice.person (taxId, name) VALUES ('83475496054', 'Felipe Almeida');
+INSERT INTO invoice.person (taxId, name) VALUES ('83355411086', 'Gabriela Souza');
+INSERT INTO invoice.person (taxId, name) VALUES ('12055213059', 'Henrique Lima');
+INSERT INTO invoice.person (taxId, name) VALUES ('88219511036', 'Isabella Rocha');
+INSERT INTO invoice.person (taxId, name) VALUES ('41594735042', 'João Mendes');
+INSERT INTO invoice.person (taxId, name) VALUES ('91590166043', 'Karina Nascimento');
+INSERT INTO invoice.person (taxId, name) VALUES ('57784985002', 'Lucas Martins');
+INSERT INTO invoice.person (taxId, name) VALUES ('42542317097', 'Mariana Torres');
+INSERT INTO invoice.person (taxId, name) VALUES ('34462067010', 'Nicolas Andrade');
+INSERT INTO invoice.person (taxId, name) VALUES ('47914828084', 'Olga Ferreira');
+INSERT INTO invoice.person (taxId, name) VALUES ('62800093005', 'Paulo Ribeiro');
+INSERT INTO invoice.person (taxId, name) VALUES ('89031100056', 'Quezia Almeida');
+INSERT INTO invoice.person (taxId, name) VALUES ('66385255069', 'Ricardo Teixeira');
+INSERT INTO invoice.person (taxId, name) VALUES ('48119162048', 'Sofia Pacheco');
+INSERT INTO invoice.person (taxId, name) VALUES ('85382975035', 'Tiago Campos');
 
 CREATE USER debezium WITH PASSWORD 'debezium';
 
